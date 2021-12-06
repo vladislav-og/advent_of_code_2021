981593670daa7fe78aadaae515ca2a12a57cde80
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2021-12-06 15:56:10 +0200

    aoc 6 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,11 +1,35 @@
 fun main() {
 
     fun part1(inputList: List<String>): Int {
-        return 0
+        val fishes = inputList.first().split(",").map { it.toInt() }.toMutableList()
+        for (i in 0 until 80) {
+            for (j in fishes.indices) {
+                var fish = fishes[j]
+                if (fish == 0) {
+                    fishes[j] = 6
+                    fishes.add(8)
+                } else {
+                    fishes[j] = fish - 1
+                }
+            }
+        }
+        return fishes.size
     }
 
-    fun part2(inputList: List<String>): Int {
-        return 0
+    fun part2(inputList: List<String>): Long {
+        val fishes = inputList.first().split(",").map { it.toInt() }.toMutableList()
+        val fishAtCurDay = mutableListOf<Long>()
+        repeat(9) { fishAtCurDay.add(0L)}
+        for (fish in fishes) {
+            fishAtCurDay[fish]++
+        }
+        for (i in 0 until 256) {
+            val dayOfCycle = i % 9
+            val nextDayOfCycle = (dayOfCycle + 7) % 9
+            fishAtCurDay[nextDayOfCycle] = fishAtCurDay[nextDayOfCycle] + fishAtCurDay[dayOfCycle]
+        }
+
+        return fishAtCurDay.sum()
     }
 
     // test if implementation meets criteria from the description, like:
